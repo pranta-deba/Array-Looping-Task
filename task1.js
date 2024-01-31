5bdfa206cbf62c2bca6f19a33ae438d87da13f16
Pranto Deb <rajprit0m@outlook.com>
2024-01-31 23:11:08 +0600

    task done

---
+++ task1.js
@@ -0,0 +1,9 @@
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+const reverseColor = [];
+
+for (const i of colors) {
+    reverseColor.unshift(i);
+}
+
+console.log(colors);
+console.log(reverseColor);
